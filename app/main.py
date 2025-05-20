dcf1101d205d0956f9c995e5475b57a87e4d1356
Samuelw7876 <samuel.calfueque99@gmail.com>
2025-05-20 17:59:53 -0400

    Version para Railway

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from app.database import Base, engine
 from app.routers import auth_routes, protected_routes
+from fastapi.responses import RedirectResponse
 
 app = FastAPI(title="FERREMAS API Integrada")
+
+@app.get("/", include_in_schema=False)
+def root():
+    return RedirectResponse(url="/docs")
 
 Base.metadata.create_all(bind=engine)
 
